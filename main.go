6c807ba2e6a879295fb8e3b7582764f6e9988af8
Mahdi Khancherli <vbha.mmk@gmail.com>
2023-09-17 00:23:30 +0330

    multithreaded

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"runtime/pprof"
+	"time"
 
 	"github.com/montanaflynn/stats"
 )
@@ -12,7 +19,7 @@ type Node struct {
 	Y int
 }
 
-type Nodes []*Node
+type Nodes []Node
 
 type Ground struct {
 	W        int
@@ -20,14 +27,15 @@ type Ground struct {
 	gateways Nodes
 }
 
-func (n *Node) Distance(other *Node) float64 {
-	return math.Sqrt(math.Pow(float64(n.Y)-float64(other.Y), 2) +
-		math.Pow(float64(n.X)-float64(other.X), 2))
+func (n Node) Distance(other Node) float64 {
+	dY := n.Y - other.Y
+	dX := n.X - other.X
+	return math.Sqrt(float64(dY*dY + dX*dX))
 }
 
 func (g *Ground) CreateGateways(gwcount int) {
 	for i := 0; i < gwcount; i++ {
-		gw := &Node{}
+		gw := Node{}
 		g.gateways = append(g.gateways, gw)
 		// for j := 0; j < i; j++ {
 		// 	g.Increment(gw)
@@ -35,29 +43,63 @@ func (g *Ground) CreateGateways(gwcount int) {
 	}
 }
 
-func (g *Ground) GrowAndMeasure(gwIndex, gwCount int) (sums, sds stats.Float64Data) {
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func (g Ground) GrowAndMeasure(gwIndex, gwCount int) (sums, sds stats.Float64Data) {
+
 	if gwIndex >= len(g.gateways) {
 		sum, sd := g.MeasureGround(gwCount)
 		// fmt.Printf("sum %v sd %v\n", sum, sd)
 		return []float64{sum}, []float64{sd}
 	}
+
+	if gwIndex == len(g.gateways)-1 {
+		// Leaf!
+		c := (g.W + 1) * (g.H + 1)
+		sums := make(chan float64, c)
+		sds := make(chan float64, c)
+
+		for {
+			go func(g Ground, gwIndex, gwCount int, sums chan float64, sds chan float64) {
+				newSums, newSds := g.GrowAndMeasure(gwIndex+1, gwCount)
+				sums <- newSums[0]
+				sds <- newSds[0]
+			}(g, gwIndex, gwCount, sums, sds)
+
+			if !g.Increment(&g.gateways[gwIndex]) {
+				break
+			}
+		}
+
+		var sumsArr []float64
+		var sdsArr []float64
+
+		for i := 0; i < c; i++ {
+			sumsArr = append(sumsArr, <-sums)
+			sdsArr = append(sdsArr, <-sds)
+		}
+
+		return sumsArr, sdsArr
+	}
+
 	for {
 		newSums, newSds := g.GrowAndMeasure(gwIndex+1, gwCount)
 		sums = append(sums, newSums...)
 		sds = append(sds, newSds...)
 
-		if !g.Increment(g.gateways[gwIndex]) {
+		if !g.Increment(&g.gateways[gwIndex]) {
 			break
 		}
-		if gwIndex < 5 {
-			fmt.Printf("GW %v: %v,%v\n", gwIndex, g.gateways[gwIndex].X, g.gateways[gwIndex].Y)
-		}
 	}
+
 	return sums, sds
 }
 
 // false if finished the travel!
-func (g *Ground) Increment(n *Node) bool {
+func (g Ground) Increment(n *Node) bool {
 	n.X += 1
 	if n.X > g.W {
 		n.Y++
@@ -70,12 +112,9 @@ func (g *Ground) Increment(n *Node) bool {
 	return true
 }
 
-func (g *Ground) MeasureNode(n *Node, gwcount int) (sum float64, sd float64) {
-	gws := g.AssignGateways(n, gwcount)
-	var dists []float64
-	for _, gw := range gws {
-		dists = append(dists, n.Distance(gw))
-	}
+func (g Ground) MeasureNode(n Node, gwcount int) (sum float64, sd float64) {
+	dists := g.GatewaysDist(n, gwcount)
+
 	sd, err := stats.StandardDeviation(dists)
 	if err != nil {
 		panic(err)
@@ -89,25 +128,31 @@ func (g *Ground) MeasureNode(n *Node, gwcount int) (sum float64, sd float64) {
 	return sum, sd
 }
 
-func (g *Ground) MeasureGround(gwcount int) (sum float64, sd float64) {
-	n := &Node{
-		X: -1,
-		Y: 0,
-	}
-	var sums []float64
-	var sds []float64
-	for g.Increment(n) {
+func (g Ground) MeasureGround(gwcount int) (sum float64, sd float64) {
+	n := Node{}
+	// c := (g.H + 1) * (g.W + 1)
+	// sums := make(chan float64, c)
+	// sds := make(chan float64, c)
+
+	var sumsArr []float64
+	var sdsArr []float64
+	for {
 		sum, sd := g.MeasureNode(n, gwcount)
-		sums = append(sums, sum)
-		sds = append(sds, sd)
+
+		sumsArr = append(sumsArr, sum)
+		sdsArr = append(sdsArr, sd)
+
+		if !g.Increment(&n) {
+			break
+		}
 	}
 
-	sum, err := stats.Mean(sums)
+	sum, err := stats.Mean(sumsArr)
 	if err != nil {
 		panic(err)
 	}
 
-	sd, err = stats.Mean(sds)
+	sd, err = stats.Mean(sdsArr)
 	if err != nil {
 		panic(err)
 	}
@@ -115,65 +160,79 @@ func (g *Ground) MeasureGround(gwcount int) (sum float64, sd float64) {
 	return sum, sd
 }
 
-func (g *Ground) AssignGateways(n *Node, gwcount int) Nodes {
-	var gws Nodes
+func (g Ground) GatewaysDist(n Node, gwcount int) []float64 {
+	var gwds []float64
 	for i := 0; i < gwcount; i++ {
-		gw := g.gateways.Nearest(n, gws)
-		if gw == nil {
-			panic("No enough gateways!")
-		}
-		gws = append(gws, gw)
+		gwd := g.gateways.NearestDist(n, gwds)
+		gwds = append(gwds, gwd)
 	}
-	return gws
+	return gwds
 }
 
-func (nodes Nodes) Nearest(n *Node, exclude Nodes) *Node {
+func (nodes Nodes) NearestDist(n Node, exclude Dists) float64 {
 	minDist := math.MaxFloat64
-	var minNode *Node
-	minNode = nil
 	for _, node := range nodes {
 		dist := n.Distance(node)
 		if dist < minDist {
-			if exclude != nil && exclude.Contains(node) {
-				exclude.Remove(node)
+			if exclude != nil && exclude.Contains(dist) {
+				exclude.Remove(dist)
 				continue
 			}
 			minDist = dist
-			minNode = node
 		}
 	}
-	return minNode
+	return minDist
 }
 
-func (nodes Nodes) Contains(n *Node) bool {
-	for _, node := range nodes {
-		if n.X == node.X && n.Y == node.Y {
+type Dists []float64
+
+func (dists Dists) Contains(dist float64) bool {
+
+	for _, d := range dists {
+		if d == dist {
 			return true
 		}
 	}
 	return false
 }
 
-func remove(slice *Nodes, s int) {
+func remove(slice *Dists, s int) {
 	*slice = append((*slice)[:s], (*slice)[s+1:]...)
 }
 
-func (nodes *Nodes) Remove(n *Node) {
-	for i, node := range *nodes {
-		if n.X == node.X && n.Y == node.Y {
-			remove(nodes, i)
-			return
+func (dists *Dists) Remove(dist float64) {
+	for i, d := range *dists {
+		if d == dist {
+			remove(dists, i)
+			break
 		}
 	}
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	debug.SetGCPercent(-1)
+	debug.SetMemoryLimit(1000 * 1000 * 1000 * 7)
+
 	g := Ground{
-		W: 10,
-		H: 10,
+		W: 100,
+		H: 100,
 	}
 
-	g.CreateGateways(4)
+	N := 8
+
+	g.CreateGateways(N)
 
 	sums, sds := g.GrowAndMeasure(0, 3)
 
